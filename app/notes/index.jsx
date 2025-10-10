c58d229aefe6840ea50271e13d71b9d0287b655e
Alumno1 <alumno1@anahuacmayab.edu.mx>
2025-10-09 20:22:09 -0600

    Sesion del jueves 09

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,9 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 const NoteScreen = () => {
+
+  const [notes, setNotes] = useState([
+    {id: '1', text: "nota uno"},
+    {id: '2', text: "nota dos"},
+    {id: '3', text: "nota tres"},
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <FlatList 
+        data={notes}
+        keyExtractor={(item)=> item.id}
+        renderItem={({item}) => (
+          <View style={styles.noteItem}>
+            <Text style={styles.noteText}>{item.text}</Text>
+          </View>
+        )}
+      />
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.addButton}>Agregar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -15,5 +34,26 @@ const styles = StyleSheet.create({
         flex:1,
         padding:20,
         backgroundColor: '#fff'
+    },
+    noteItem: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor: '#7d4d0480',
+      padding:15,
+      borderRadius:5,
+      marginVertical:5
+    },
+    noteText: {
+      fontSize:18
+    },
+    addButton: {
+      position: 'absolute',
+      bottom: 20,
+      left: 20,
+      right:20,
+      backgroundColor: '#666',
+      padding:15,
+      borderRadius:5,
+      alignItems:'center'
     }
 })
